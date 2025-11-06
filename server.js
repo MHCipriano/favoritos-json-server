a533674281228326e7857374c4051d5d1a449f79
MHCipriano <116772856+MHCipriano@users.noreply.github.com>
2025-11-06 00:26:23 -0400

    reajuste do server.js para habilitar CORS no json-server

--- server.js
+++ server.js
@@ -11,6 +11,21 @@ const router = jsonServer.router(db);
 
 const middlewares = jsonServer.defaults();
 
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+  );
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+
 server.use(middlewares);
 server.use(
   jsonServer.rewriter({
